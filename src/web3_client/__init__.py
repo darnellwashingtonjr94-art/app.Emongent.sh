28d2f15764dff734609963969262ed1c1addfd70
Darion Kellar <credkellar@gmail.com>
2026-05-19 02:09:43 -0700

    Update __init__.py

--- src/web3_client/__init__.py
+++ src/web3_client/__init__.py
@@ -1,13 +1,12 @@
 import os
-from web3 import AsyncWeb3
-from web3.providers.async_rpc import AsyncHTTPProvider
+from web3 import AsyncWeb3, AsyncHTTPProvider
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class MonadClient:
     def __init__(self):
-        # Defaulting to a local/devnet RPC if environment variable isn't set
+        # Defaulting to local/devnet RPC if environment variable isn't set
         self.rpc_url = os.getenv("MONAD_RPC_URL", "http://127.0.0.1:8545")
         self.w3 = AsyncWeb3(AsyncHTTPProvider(self.rpc_url))
 
